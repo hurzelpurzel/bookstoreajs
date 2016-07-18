e6381bcad9f567e79779e4aecbb0e373cdc23a20
hurzelpurzel <ludger.pottmeier@gmx.de>
2016-07-18 14:53:22 +0200

    just odata

--- service/odataserver.js
+++ service/odataserver.js
@@ -1,7 +1,9 @@
 var odata = require('node-odata');
+var fs = require('fs');
 
 var _server = odata('mongodb://localhost/odata-test');
 var  _odataPort =3000;
+var _filePath = __dirname+"/files";
 
 var bookInfo = {
   author: String,
@@ -12,6 +14,9 @@ var bookInfo = {
   title: String
 };
 
+var UploadFile = function(id, content){
+			return {id: id, content: content}; 
+};
 
 	
 _server.resource('BookSet', bookInfo)
@@ -27,6 +32,20 @@ _server.resource('BookSet', bookInfo)
 _server.get('/license', function(req, res, next){
   res.jsonp({license:'MIT'});
 });
+_server.get('/getFile',function(req, res, next){
+  fs.readFile(_filePath+"/"+req.params.id+'.tmp', function (err,data) {
+  if (err) return console.log(err);
+  res.json(new UploadFile(id, data));
+});
+});
+_server.put('/putFile',function(req, res, next){
+  
+fs.writeFile(_filePath+"/"+req.body.id+'.tmp', req.body.content, function (err) {
+  if (err) return console.log(err);
+  console.log('put File'+_filePath+"/"+req.body.id+'.tmp succeed');
+});
+});
+
 _server.listen(_odataPort, function(){
   console.log('OData services has started, you can visit by http://localhost:'+_odataPort+'/BookSet');
 });
